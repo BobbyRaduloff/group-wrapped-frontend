68fb4fdfc1a6d3c7a933f3027c721ba73b0bc99d
Ognian Trajanov Jr. <ogtrjr@gmail.com>
2025-05-13 18:19:04 -0400

    more neutral theming

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 export const Route = createRootRoute({
   component: () => (
     <div className="min-h-screen w-full bg-[url(/background.webp)] bg-cover cursor-default relative">
-      <div className="absolute inset-0 bg-black/20"></div>
+      <div className="absolute inset-0 bg-black/10"></div>
       <div className="relative z-10">
         <Outlet />
       </div>
